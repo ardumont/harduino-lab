148458343cae8ccd328719edf2c5f93fab1e2069
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-08 20:28:17 +0100

    Open the means to make the led blink from the shell.

--- src/blink.hs
+++ src/blink.hs
@@ -3,6 +3,7 @@ module Blink where
 import Control.Monad (forever)
 
 import System.Hardware.Arduino
+import System.Environment
 
 -- prepare the led
 prepareLed :: Pin -> Arduino ()
@@ -16,8 +17,17 @@ blink l = do digitalWrite l True
              delay 1000
 
 -- Blink the led connected to port 13 on the Arduino UNO board.
+run :: String -> IO ()
+run device = withArduino False device $ do
+               let led = digital 13
+               prepareLed led
+               forever $ blink led
+
+-- *Blink> run "/dev/ttyACM0"
+--   C-c C-chArduino: Caught Ctrl-C, quitting..
+
+-- run from the cli
 main :: IO ()
-main = withArduino False "/dev/ttyACM0" $ do
-          let led = digital 13
-          prepareLed led
-          forever $ blink led
+main =
+  do (devicePath:_) <- getArgs
+     run devicePath
