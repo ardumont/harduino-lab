f322510f3e8480c919109324d5628ae4bfdfd480
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-08 20:32:44 +0100

    Use constant time pause

--- src/blink.hs
+++ src/blink.hs
@@ -5,6 +5,9 @@ import Control.Monad (forever)
 import System.Hardware.Arduino
 import System.Environment
 
+pauseTime :: Int
+pauseTime = 500
+
 -- prepare the led
 prepareLed :: Pin -> Arduino ()
 prepareLed = flip setPinMode OUTPUT
@@ -12,9 +15,9 @@ prepareLed = flip setPinMode OUTPUT
 -- make the led blink
 blink :: Pin -> Arduino ()
 blink l = do digitalWrite l True
-             delay 1000
+             delay pauseTime
              digitalWrite l False
-             delay 1000
+             delay pauseTime
 
 -- Blink the led connected to port 13 on the Arduino UNO board.
 run :: String -> IO ()
